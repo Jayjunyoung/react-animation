b5a556cadf64cab57e89a7a4868d77629a105da5
junyoung <no2jfamily@tukorea.ac.kr>
2022-09-13 16:03:50 +0900

    0913

--- src/App5.tsx
+++ src/App5.tsx
@@ -1,12 +1,12 @@
 import styled from "styled-components";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion} from "framer-motion";
 import { useState } from "react";
 
 const Wrapper = styled(motion.div)`
     height: 100vh;
     display: flex;
     width: 100vw;
-    justify-content: center;
+    justify-content: space-around;
     align-items: center;
     flex-direction: column;
 `;
@@ -14,74 +14,40 @@ const Wrapper = styled(motion.div)`
 
 const Box = styled(motion.div)`
     width: 400px;
-    height: 200px;
+    height: 400px;
     background-color: rgba(255, 255, 255, 1);
     border-radius: 40px;
-    position: absolute;//슬라이더가 안튕기로도록한것
-    top: 100px;
     display: flex;
     justify-content: center;
     align-items: center;
-    font-size: 28px;
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const box = {//custom은 함수를 이용해 객체반환
-    entry: (back:boolean) => {
-        return {
-            x: back? -500 : 500,//안보일땐 x:500인 방향에있음
-            opacity: 0,
-            scale: 0,
-        }
-    },
-    center: {
-        x: 0,//보일땐 가운데에 있을것
-        opacity: 1,
-        scale: 1,
-        transition: {
-            duration: 0.3,
-        },
-    },
-    exit: (back:boolean) => { 
-        return {
-            x: back? 500 : -500, //true일때 500으로 날라감
-            opacity: 0, 
-            scale: 0, 
-            transition: { duration: 0.3 }
-        }
-    },
-};
+const Circle = styled(motion.div)`
+    background-color: #00a5ff;
+    height: 100px;
+    width: 100px;
+    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
 
 
 function App5() {
-    const [visible, setVisible] = useState(1);
-    const [back,setBack] = useState(false);
-    const nextPlease = () => {
-        setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-        setBack(false);
-    }
-    const prevPlease = () => {
-        setVisible((prev) => (prev === 1 ? 1 : prev - 1));
-        setBack(true)
-    }//exitBeforeEnter - 노트에 적어놈
-    return (<Wrapper>
-        <AnimatePresence custom={back}>
-            <Box
-                custom={back}
-                variants={box}
-                initial="entry"
-                animate="center"
-                exit="exit"
-                key={visible}//이거에따라 렌더링이됌
-            >
-                {visible}
-            </Box>
-        </AnimatePresence>
-        <button onClick={nextPlease}>next</button>
-        <button onClick={prevPlease}>prev</button>
+    const [clicked, setClicked] = useState(false);
+    const toggleClicked = () => setClicked((prev) => !prev);
+    return (
+        <Wrapper onClick={toggleClicked}>
+        <Box>
+        {!clicked ? (//false일땐 원형이됌
+            <Circle layoutId="circle" style={{ borderRadius: 50 }} />
+        ) : null}
+        </Box>
+        <Box>
+        {clicked ? (
+            <Circle layoutId="circle" style={{ borderRadius: 0, scale: 2 }} />
+        ) : null}
+        </Box>
     </Wrapper>
-    )//motion.을통해 애니메이션 주고자하는
-    //html태그 적용 가능
+    )//layoutId: 두개가 같은 컴포넌트라고 알려주는것
 }
 
 export default App5;
