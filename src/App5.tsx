0d43cbde990f5eb1b59f4ca8215ca53c0de6a78c
junyoung <no2jfamily@tukorea.ac.kr>
2022-09-13 16:36:01 +0900

    7.15

--- src/App5.tsx
+++ src/App5.tsx
@@ -1,53 +1,75 @@
 import styled from "styled-components";
-import { motion} from "framer-motion";
+import { motion, AnimatePresence} from "framer-motion";
 import { useState } from "react";
 
-const Wrapper = styled(motion.div)`
+const Wrapper = styled.div`
     height: 100vh;
-    display: flex;
     width: 100vw;
+    display: flex;
     justify-content: space-around;
     align-items: center;
-    flex-direction: column;
+`;
+
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    width: 50vw;
+    gap: 10px;
+    div:first-child,
+    div:last-child {
+    grid-column: span 2;
+    }
 `;
 
 
 const Box = styled(motion.div)`
-    width: 400px;
-    height: 400px;
     background-color: rgba(255, 255, 255, 1);
     border-radius: 40px;
+    height: 200px;
+    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Overlay = styled(motion.div)`
+    //누르면 가운데로 오는 레이아웃
+    width: 100%;
+    height: 100%;
+    position: absolute;//안눌린 애들 위에 있어야함
     display: flex;
     justify-content: center;
     align-items: center;
-    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
+`
 
-const Circle = styled(motion.div)`
-    background-color: #00a5ff;
-    height: 100px;
-    width: 100px;
-    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
+const overlay = {
+    hidden: { backgroundColor: "rgba(0, 0, 0, 0)" },
+    visible: { backgroundColor: "rgba(0, 0, 0, 0.5)" },
+    exit: { backgroundColor: "rgba(0, 0, 0, 0)" },
+};
 
 
-function App5() {
-    const [clicked, setClicked] = useState(false);
-    const toggleClicked = () => setClicked((prev) => !prev);
+function App5() {//null: 아무것도선택X string: "1" ~ "4"
+    const [id, setId] = useState<null | string>(null);//기본값 null
     return (
-        <Wrapper onClick={toggleClicked}>
-        <Box>
-        {!clicked ? (//false일땐 원형이됌
-            <Circle layoutId="circle" style={{ borderRadius: 50 }} />
+        <Wrapper>
+        <Grid>
+        {["1", "2", "3", "4"].map((n) => (
+            <Box onClick={() => setId(n)} key={n} layoutId={n} />
+        ))}
+        </Grid>
+        <AnimatePresence>
+        {id ? (//박스를 넣어줄 오버 레이아웃 공간
+            <Overlay
+            variants={overlay}
+            onClick={() => setId(null)}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            >
+                <Box layoutId={id} style={{ width: 400, height: 200 }} />
+            </Overlay>
         ) : null}
-        </Box>
-        <Box>
-        {clicked ? (
-            <Circle layoutId="circle" style={{ borderRadius: 0, scale: 2 }} />
-        ) : null}
-        </Box>
+        </AnimatePresence>
     </Wrapper>
-    )//layoutId: 두개가 같은 컴포넌트라고 알려주는것
+    )
 }
 
 export default App5;
