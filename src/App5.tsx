40f09b0b6bead5ef95f90d38562fd8d74200bd32
junyoung <no2jfamily@tukorea.ac.kr>
2022-09-13 14:26:54 +0900

    7.10

--- src/App5.tsx
+++ src/App5.tsx
@@ -1,49 +1,54 @@
 import styled from "styled-components";
-import { useRef } from "react";
-import { 
-    motion, 
-    useMotionValue, 
-    useTransform,
-    useViewportScroll, 
-    } from "framer-motion";
-
+import { motion } from "framer-motion";
 
 const Wrapper = styled(motion.div)`
-    height: 200vh;
+    height: 100vh;
     display: flex;
     width: 100vw;
     justify-content: center;
     align-items: center;
 `;
 
-const Box = styled(motion.div)`//애니메이션된 스타일컴포넌트적용방법
-    width: 200px;
-    height: 200px;
-    background-color: rgba(255, 255, 255, 1);
-    border-radius: 10px;
-    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const Svg = styled.svg`
+    //path는 Svg의 color를씀
+    width: 300px;
+    height: 300px;
+    path {
+        stroke: white;
+        stroke-width: 2;//선의두께
+    }
 `;
 
+const svg = {
+    start: { pathLength: 0, fill: "rgba(255, 255, 255, 0)" },
+    end: {
+        fill: "rgba(255, 255, 255, 1)",
+        pathLength: 1,//선이 다채워짐
+    },
+};
+
 function App5() {
-    const x = useMotionValue(0);//style의 x로연걸
-    //첫배열: -800 , 0 , 800의 값을 원함, 두번째: 그에해당하는크기
-    //const scale = useTransform(x, [-800, 0, 800], [2, 1, 0.1]);
-    //motionvalue는 바껴도 재렌더링 되지않는다
-    const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
-    const gradient = useTransform(//배경색 건드리는것
-        x,
-        [-800, 800],
-        [
-            "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
-            "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
-        ]
-    );
-    const { scrollYProgress } = useViewportScroll();
-    //0:상단 1:하단
-    const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
-    return (<Wrapper style={{ background: gradient }}>
-                <Box style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin />
-            </Wrapper>
+    return (<Wrapper>
+        <Svg
+            focusable="false"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+        >
+            <motion.path
+            variants={svg}
+            initial="start"
+            animate="end"
+            transition={{
+                default: { duration: 5 },
+                //전체가 되는게 5초가걸려
+                fill: { duration: 1, delay: 3 },
+                //색을 채우는건 선이그려지고 3초이따 할것
+                //trasition을 여기에씀으로써 fill을 따로통제
+            }}
+            d="M224 373.12c-25.24-31.67-40.08-59.43-45-83.18-22.55-88 112.61-88 90.06 0-5.45 24.25-20.29 52-45 83.18zm138.15 73.23c-42.06 18.31-83.67-10.88-119.3-50.47 103.9-130.07 46.11-200-18.85-200-54.92 0-85.16 46.51-73.28 100.5 6.93 29.19 25.23 62.39 54.43 99.5-32.53 36.05-60.55 52.69-85.15 54.92-50 7.43-89.11-41.06-71.3-91.09 15.1-39.16 111.72-231.18 115.87-241.56 15.75-30.07 25.56-57.4 59.38-57.4 32.34 0 43.4 25.94 60.37 59.87 36 70.62 89.35 177.48 114.84 239.09 13.17 33.07-1.37 71.29-37.01 86.64zm47-136.12C280.27 35.93 273.13 32 224 32c-45.52 0-64.87 31.67-84.66 72.79C33.18 317.1 22.89 347.19 22 349.81-3.22 419.14 48.74 480 111.63 480c21.71 0 60.61-6.06 112.37-62.4 58.68 63.78 101.26 62.4 112.37 62.4 62.89.05 114.85-60.86 89.61-130.19.02-3.89-16.82-38.9-16.82-39.58z"
+        ></motion.path>
+        </Svg>
+    </Wrapper>
     )//motion.을통해 애니메이션 주고자하는
     //html태그 적용 가능
 }
